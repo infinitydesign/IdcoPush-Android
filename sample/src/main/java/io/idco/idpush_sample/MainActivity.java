efdf3541aff93d868571c633876228a37615f258
Hamidreza Etebarian <shetmobile@gmail.com>
2019-06-30 16:09:05 +0430

    build.gradle updated dependencies

--- sample/src/main/java/io/idco/idpush_sample/MainActivity.java
+++ sample/src/main/java/io/idco/idpush_sample/MainActivity.java
@@ -1,7 +1,8 @@
 package io.idco.idpush_sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
